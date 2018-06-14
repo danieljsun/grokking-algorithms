37f88b4c0e7bf6e27f8d5814b07351e9ea8890bc
Daniel Sun <danieljsun@gmail.com>
2018-06-14 13:19:16 +0000

    fixed up selection sort

--- algo/selection-sort.go
+++ algo/selection-sort.go
@@ -1,12 +1,39 @@
 package algo
 
-func SelectionSort(s []int) []int {
+import "fmt"
+
+func WhatSort(s []int) []int {
 	for i, _ := range s {
 		for j := i + 1; j < len(s); j++ {
 			if (s[i] > s[j]) {
 				s[i], s[j] = s[j], s[i]
 			}
 		}
+	}
+	return s
+}
+
+// Returns the index of the smallest number in the array
+func findSmallest(s []int) (smallestIndex int) {
+	smallest := s[smallestIndex]
+	for i := 1; i < len(s); i++ {
+		if s[i] < smallest {
+			smallestIndex = i
+			smallest = s[i]
+		}
+	}
+	fmt.Println("smallestIndex ", smallestIndex, ", smallest ", smallest)
+	return
+}
+
+func SelectionSort(s []int) []int {
+	for i, _ := range s {
+		fmt.Println("Working on ", i);
+		smallestIndex := findSmallest(s[i:])
+		if smallestIndex > 0 {
+			s[i], s[i + smallestIndex] = s[i + smallestIndex], s[i]
+		}
+		fmt.Println("After worked on ", i, " ", s);
 	}
 	return s
 }
